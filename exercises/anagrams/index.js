2d8edb89b6340bda9c47e7a5d9fb15795f7b5889
Yulong Duan <denis.duan@DiMac.gateway>
2019-04-29 23:15:46 +1000

    Finished anagram exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+// solution two:
+function anagrams(stringA, stringB) {
+    comparableStrA = buildComparableStr(stringA);
+    comparableStrB = buildComparableStr(stringB);
+    return comparableStrA === comparableStrB;
+}
+
+// This function builds a string that's easier to be compared with
+function buildComparableStr(str) {
+    return str.toLowerCase().replace(/[^\w]/g, '').split('').sort().join('');
+}
+
+// Solution One: 
+
+// function anagrams(stringA, stringB) {
+//     charMapA = buildCharMap(stringA);
+//     charMapB = buildCharMap(stringB);
+
+//     if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//         return false
+//     }
+
+//     for (let key in charMapA) {
+//         if (charMapA[key] !== charMapB[key]) {
+//             return false
+//         }
+//     }
+
+//     return true
+// }
+
+// function buildCharMap(str) {
+//     var charMap = {};
+//     for (let c of str.toLowerCase().replace(/[^\w]/g, '')) {
+//         charMap[c] = charMap[c] + 1 || 1
+//     }
+//     return charMap;
+// }
 
 module.exports = anagrams;

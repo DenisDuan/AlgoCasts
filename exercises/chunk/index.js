b4027d60fe3d5293e167658bc88123f221d2dc6b
Yulong Duan <denis.duan@DiMac.gateway>
2019-05-06 23:39:00 +1000

    Finished exercise of chunks

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,47 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// solution Two - using slice 
+function chunk(array, size) {
+    const chunks = [];
+    let startIndex = 0;
+    while (array.length > startIndex) {
+        // This is too much, a nice property of slice is that: if end is greater than the length of the sequence, slice extracts through to the end of the sequence.
+        // So, this code can be simply as the example solution
+        let endIndex = (startIndex + size) > array.length ? array.length : startIndex + size 
+        chunks.push(array.slice(startIndex, endIndex));
+        startIndex = startIndex + size;
+    }
+    return chunks;
+}
+/**
+ * example solution
+ * 
+ * 
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
+ */
+
+// solution One -  Keep shifting the array, and put it into a new array up to the chunck size. Push each array into a array.
+function chunk_one(array, size) {
+    const chunks = [];
+    while (array.length > 0) {
+        let chunk = [];
+        for (let counter = size; counter > 0 && array.length > 0; counter--) {
+            chunk.push(array.shift());
+        }
+        chunks.push(chunk);
+    }
+    return chunks;
+}
 
 module.exports = chunk;
